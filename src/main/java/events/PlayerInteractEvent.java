2bd848c90993bb3fcaa3ea9cc461ea70b312d399
Kai Welsing <kai.welsing@googlemail.com>
2025-01-08 11:17:40 +0100

    Updated Java version to 21. Updated to minecraft Version 1.21.3. Switchd from Towny to Lands support. New version number 2.5

--- src/main/java/events/PlayerInteractEvent.java
+++ src/main/java/events/PlayerInteractEvent.java
@@ -1,28 +1,25 @@
 package events;
 
-import com.palmergames.bukkit.towny.object.TownyPermission;
-import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
+
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import commands.AddTool;
-import de.ewu2000.galdreenblocksunlimited.CustomBlock;
 import de.ewu2000.galdreenblocksunlimited.CustomBlockCompound;
-import de.ewu2000.galdreenblocksunlimited.CustomBlockCycle;
 import de.ewu2000.galdreenblocksunlimited.GaldreenBlocksUnlimited;
+import me.angeschossen.lands.api.LandsIntegration;
+import me.angeschossen.lands.api.flags.type.Flags;
+import me.angeschossen.lands.api.flags.type.RoleFlag;
+import me.angeschossen.lands.api.land.LandWorld;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
-import com.palmergames.bukkit.towny.TownyAPI;
-import org.bukkit.permissions.Permission;
 
 import java.lang.reflect.Type;
 
@@ -33,11 +30,22 @@ public class PlayerInteractEvent implements Listener {
         RegionQuery query = WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery();
         com.sk89q.worldedit.util.Location loc = BukkitAdapter.adapt(event.getClickedBlock().getLocation());
         com.sk89q.worldedit.world.World world = BukkitAdapter.adapt(event.getPlayer().getWorld());
-        boolean wgResult = query.testState(loc, WorldGuardPlugin.inst().wrapPlayer(event.getPlayer()), Flags.BUILD);
+        boolean wgResult = query.testState(loc, WorldGuardPlugin.inst().wrapPlayer(event.getPlayer()), com.sk89q.worldguard.protection.flags.Flags.BUILD);
         boolean canBypass = WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(WorldGuardPlugin.inst().wrapPlayer(event.getPlayer()), world);
 
+        LandsIntegration landsApi = LandsIntegration.of(GaldreenBlocksUnlimited.plugin);
+        LandWorld landsWorld = landsApi.getWorld(event.getClickedBlock().getWorld());
+        boolean bBuild = false;
+        if (landsWorld != null) { // Lands is enabled in this world
+            if (landsWorld.hasRoleFlag(event.getPlayer().getUniqueId(), event.getClickedBlock().getLocation(), Flags.BLOCK_BREAK)) {
+                //if lands allows building
+                bBuild = true;
+            }
+        }
+
         //check if towny allows building for this player
-        boolean bBuild = PlayerCacheUtil.getCachePermission(event.getPlayer(), event.getClickedBlock().getLocation(), event.getClickedBlock().getType(), TownyPermission.ActionType.BUILD);
+        //boolean bBuild = PlayerCacheUtil.getCachePermission(event.getPlayer(), event.getClickedBlock().getLocation(), event.getClickedBlock().getType(), TownyPermission.ActionType.BUILD);
+
         if(event.getHand() == EquipmentSlot.OFF_HAND){
             return;
         }
@@ -47,6 +55,10 @@ public class PlayerInteractEvent implements Listener {
         if (event.useInteractedBlock() == Event.Result.DENY) { //Not denied by plot plugin
             return;
         }
+        if (event.useItemInHand() == Event.Result.DENY) {
+            return;
+        }
+
         if (event.getPlayer().getInventory().getItemInMainHand().isSimilar(AddTool.tool)) { //changeTool
             if (!(event.getAction().isRightClick() && event.getClickedBlock() != null && !event.getPlayer().isSneaking())) {
                 return;
